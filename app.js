0752111b698da3c6f26b874bc28df8d184f86d62
FranciscoBM <baronfr18@gmail.com>
2025-11-09 20:55:14 -0600

    feat(validation): implement validation functions and types for GraphQL schema
    - Added `validate`, `validateSDL`, `assertValidSDL`, and `assertValidSDLExtension` functions to handle schema validation.

--- app.js
+++ app.js
@@ -1,12 +1,64 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {graphqlHTTP} = require('express-graphql');
+
+const { buildSchema } = require('graphql');
 
 const app = express();
 
+const expenses = [];
+
 app.use(bodyParser.json());
 
-app.get('/',(req,res,next)=>{
-  res.send('Hello World!');
-})
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(`
+
+      type Expense{
+        _id: ID!
+        title: String!
+        description: String!
+        amount: Float!
+        date: String!      
+      }
+
+      input ExpenseInput{
+        title: String!
+        description: String!
+        amount: Float!
+        date: String!
+      }
+
+      type RootQuery{
+        expenses: [Expense!]!
+      }
+
+      type RootMutation{
+        createExpense(expenseInput: ExpenseInput): Expense
+      }
+
+      schema{
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      expenses: () => {
+        return expenses;
+      },
+      createExpense: (args) => {
+        const expense = {
+          _id: Math.random().toString(),
+          title: args.title,
+          description: args.description,
+          amount: +args.amount,
+          date: new Date().toISOString
+        }
+        expenses.push(expense);
+      }
+    },
+    graphiql: true
+  }));
 
 app.listen(3000);
